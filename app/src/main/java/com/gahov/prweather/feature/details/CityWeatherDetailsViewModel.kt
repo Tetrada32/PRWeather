a2829435c30ca549cd301970fc0b95c69e809bf5
danilgahov <danylo.hakhov@onix-systems.com>
2023-08-13 17:03:00 +0200

    add loading bar in details

--- app/src/main/java/com/gahov/prweather/feature/details/CityWeatherDetailsViewModel.kt
+++ app/src/main/java/com/gahov/prweather/feature/details/CityWeatherDetailsViewModel.kt
@@ -8,6 +8,7 @@ import com.gahov.prweather.domain.entities.weather.CityWeatherParams
 import com.gahov.prweather.domain.entities.weather.WeatherEntity
 import com.gahov.prweather.domain.usecase.weather.LoadRemoteCityWeatherUseCase
 import com.gahov.prweather.feature.details.command.CityWeatherDetailsCommand
+import com.gahov.prweather.feature.details.entity.WeatherDetailsDataModel
 import com.gahov.prweather.feature.details.factory.WeatherEntityToModelBuilder
 import com.gahov.prweather.feature.details.presenter.CityWeatherDetailsPresenter
 import kotlinx.coroutines.Dispatchers
@@ -18,7 +19,11 @@ class CityWeatherDetailsViewModel @Inject constructor(
     private val loadCityWeatherUseCase: LoadRemoteCityWeatherUseCase
 ) : BaseViewModel(), CityWeatherDetailsPresenter {
 
+    var model = WeatherDetailsDataModel()
+
     fun loadContent(args: CityWeatherDetailsFragmentArgs) {
+        submitList(true)
+
         val cityData = args.cityWeatherData
         val cityName = args.cityName
 
@@ -40,12 +45,18 @@ class CityWeatherDetailsViewModel @Inject constructor(
     }
 
     private fun onResultSuccess(result: WeatherEntity) {
-        handleCommand(
-            CityWeatherDetailsCommand.DisplayContent(modelBuilder.buildWeatherModel(result))
-        )
+        model = modelBuilder.buildWeatherModel(result)
+        submitList(false)
+    }
+
+    private fun submitList(isLoading: Boolean) {
+        model.isLoading.set(isLoading)
+        handleCommand(CityWeatherDetailsCommand.DisplayContent(model))
     }
 
     private fun onResultFailure(failureResult: Failure) {
+        model = WeatherDetailsDataModel()
+        submitList(false)
         handleCommand(CityWeatherDetailsCommand.OnNetworkError(failureResult))
     }
 
--- app/src/main/java/com/gahov/prweather/feature/details/entity/WeatherDetailsDataModel.kt
+++ app/src/main/java/com/gahov/prweather/feature/details/entity/WeatherDetailsDataModel.kt
@@ -1,5 +1,6 @@
 package com.gahov.prweather.feature.details.entity
 
+import androidx.databinding.ObservableBoolean
 import com.gahov.prweather.arch.ui.view.model.IconProvider
 import com.gahov.prweather.arch.ui.view.model.TextProvider
 
@@ -11,5 +12,6 @@ data class WeatherDetailsDataModel(
     val mainTemperature: TextProvider? = null,
     val weatherFields: List<WeatherDetailsFieldModel>? = null,
     val weatherDate: TextProvider? = null,
-    val requestTime: TextProvider? = null
+    val requestTime: TextProvider? = null,
+    var isLoading: ObservableBoolean = ObservableBoolean(true)
 )
